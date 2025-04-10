88d6925b57fc0b2ea5f8bf836aec8d8e552be672
weiw <290712411@qq.com>
2025-04-10 11:18:26 +0800

    update hello.java

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -13,7 +13,7 @@ public class HelloWorld {
 
     public static void main(String[] arg) {
         Spark.get("/hello", (request, response) -> {
-            return "Hello 4444444444444444  wwworld!";
+            return "Hello 555555555555  wwworld!";
         });
     }
 }
